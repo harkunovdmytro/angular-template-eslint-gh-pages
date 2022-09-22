42a42d9c3e308daaaafca7b1875fe70173576f12
harkunovdmytro <harkunovdmytro@gmail.com>
2022-09-22 15:48:59 +0300

    added angular material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatButtonModule} from "@angular/material/button";
+
+export const materialModules = []
+
+@NgModule({
+  imports: [
+    CommonModule,
+    materialModules,
+  ],
+  exports: [
+    materialModules,
+  ]
+})
+export class MaterialModule {
+}
