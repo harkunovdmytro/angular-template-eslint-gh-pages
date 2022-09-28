deed215835fab86b14bb809bfd2e9c7ec2f0161a
harkunovdmytro <harkunovdmytro@gmail.com>
2022-09-28 14:10:05 +0300

    added prettier and husky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppComponent} from './app.component';
-import {MaterialModule} from "./material/material.module";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,11 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MatButtonModule} from "@angular/material/button";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
-export const materialModules = []
+export const materialModules = [MatButtonModule];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    materialModules,
-  ],
-  exports: [
-    materialModules,
-  ]
+  imports: [CommonModule, materialModules],
+  exports: [materialModules],
 })
-export class MaterialModule {
-}
+export class MaterialModule {}
